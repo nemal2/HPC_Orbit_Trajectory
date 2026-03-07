26f6cbf351cdecaddfb3b0fa28c8c3a033d229f8
Nemal Perera <122453475+nemal2@users.noreply.github.com>
2026-03-07 10:18:36 +0530

    Initial commit

---
+++ rocket_serial.c
@@ -0,0 +1,12 @@
+//SERIAL VERSION - Rocket Trajectory Optimization
+
+// pseudocode
+
+//== 1. Initialize random population of trajectories
+//== 2. For each trajectory
+//==       simulate rocket flight
+//==       calculate physics
+//==       evaluate fitness
+//== 3. Compare all trajectories
+//== 4. Select the best trajectory
+//== 5. Print results
