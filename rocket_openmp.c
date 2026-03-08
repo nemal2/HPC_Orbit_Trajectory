5ae1ef5a9165937745f8a39693073e5fa14ff101
Jayani Perera <pererajayani27@gmail.com>
2026-03-08 16:47:12 +0530

    Implemented the serial loop to understand the problem.

--- rocket_openmp.c
+++ rocket_openmp.c
@@ -1,6 +1,26 @@
 #include "rocket_trajectory.h"
 #include <omp.h>
 
+TrajectoryResult find_best_trajectory_serial_test(ControlProfile *population, int size)
+{
+    TrajectoryResult best;
+    best.fitness = 1e9;
+
+    for (int i = 0; i < size; i++)
+    {
+        TrajectoryResult result = simulate_trajectory(&population[i], i);
+        calculate_fitness(&result);
+
+        if (result.fitness < best.fitness)
+            best = result;
+
+        if ((i + 1) % 100 == 0)
+            printf("Tested %d/%d trajectories\n", i + 1, size);
+    }
+
+    return best;
+}
+
 int main(int argc, char *argv[])
 {
     printf("ROCKET TRAJECTORY OPTIMIZATION - OPENMP VERSION\n\n");
@@ -19,6 +39,15 @@ int main(int argc, char *argv[])
 
     printf("Population of %d trajectories initialized\n", POPULATION_SIZE);
 
+    double start_time = omp_get_wtime();
+    TrajectoryResult best = find_best_trajectory_serial_test(population, POPULATION_SIZE);
+    double exec_time = omp_get_wtime() - start_time;
+
+    printf("\nBest trajectory ID: %d\n", best.trajectory_id);
+    printf("Fuel consumed: %.2f kg\n", best.fuel_consumed);
+    printf("Execution time: %.3f seconds\n", exec_time);
+
     free(population);
+
     return 0;
 }
